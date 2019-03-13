fb1fc9c69061c1d5385bda7376ca321c89ac74ff
David Cantoran <davidcantoran36@gmail.com>
2019-03-13 16:52:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
 if phrase != phrase.upcase
-  return "NO, NOT SINCE 1938!"
+  return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 else 
